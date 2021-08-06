00807beb73196aaf36dd242c312cf92e6f63e9da
Wilian Cesar <wilcesar@live.com>
2021-08-06 16:04:53 -0300

    Subindo o projeto rest
    Subindo o projeto rest, gerado a partir do https://start.spring.io/, com as dependencias Spring Web, Spring Data JPA e MySQL Driver

---
+++ nba-rest-api/src/test/java/br/com/nbarestapi/NbaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nbarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
